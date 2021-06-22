6e14a309bfae06ffa6f5dd92ece54b42a27a66d1
Mohamed Ezzaouia <ezzaouia.mohamed@gmail.com>
2021-06-22 17:02:14 +0200

    front all

---
+++ front/src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  loglevel: 0,
+  hmr: false,
+  API_URL: "https://json-server.ezzaouia.now.sh",
+  SUIVI_STATS_URL: "https://statistiques.projet-voltaire.fr"
+};
